59d8b026900d6eebdc94db2ece864221043c910a
clemlak <clementlakhal@gmail.com>
2022-10-22 23:40:21 +0400

    test: add HelloWorld test

---
+++ test/HelloWorld.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract HelloWorldTest is Test {
+    HelloWorld public helloWorld;
+
+    function setUp() public {
+        helloWorld = HelloWorld(HuffDeployer.deploy("HelloWorld"));
+    }
+
+    function testHelloWorld() public {
+      string memory a = "Hello, world!";
+      string memory b = helloWorld.helloWorld();
+      assertEq(a, b);
+    }
+}
+
+interface HelloWorld {
+    function helloWorld() external returns (string memory);
+}
